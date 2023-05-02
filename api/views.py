27910f730128e3fa62c4243b3b0a9a2057acddad
AldiyarIkhsan <usoul31@gmail.com>
2023-05-02 17:08:19 +0500

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import redirect, render
+from rest_framework.generics import ListAPIView,CreateAPIView
+
+from django.views import View
+from django.conf import settings
+
+from .models import Link
+from .serializer import LinkSerializer
+
+class ShortenerListAPIView(ListAPIView):
+    queryset=Link.objects.all()
+    serializer_class=LinkSerializer
+
+class ShortenerCreateApiView(CreateAPIView):
+    serializer_class=LinkSerializer
+
+class Redirector(View):
+    def get(self,request,shortener_link,*args, **kwargs):
+        shortener_link=settings.HOST_URL+'/'+self.kwargs['shortener_link']
+        redirect_link=Link.objects.filter(shortened_link=shortener_link).first().original_link
+        return redirect(redirect_link)
---
+++ shortner/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from api.views import Redirector
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Bitly.com API clone",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('admin/', admin.site.urls),
+   path('api/',include('api.urls',namespace='api')),
+   path('<str:shortener_link>/',Redirector.as_view(),name='redirector')
+]
